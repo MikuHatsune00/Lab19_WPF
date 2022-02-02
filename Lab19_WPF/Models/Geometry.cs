c747539b681ceb1e257d5bf5ec70c7b8d10c07d8
Kseniia <balerinagizelle@mail.ru>
2022-02-02 11:43:26 +0300

    Add project files.

---
+++ Lab19_WPF/Models/Geometry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19_WPF.Models
+{
+    static class Geometry
+    {
+        public static int GetL(int radius) => (int)(2 * Math.PI * radius);
+    }
+}
---
+++ Lab19_WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Lab19_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab19_WPF.ViewModels
+{
+    class MainWindowViewModel:INotifyPropertyChanged
+
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName)); }
+       
+
+        private int r;
+        public int R
+        { get => r;
+        set
+            {
+                r = value;
+                OnPropertyChanged();
+            }
+        }
+        private int l;
+        public int L
+        {
+            get => l;
+            set
+            {
+                l = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand GetLCommand { get; }
+        private void OnGetLCommandExecute(object p)
+        {
+            L = Geometry.GetL(R);
+
+        }
+        private bool CanGetLCommandExecuted(object p)
+        {
+            if (r != 0)
+                return true;
+            else return false;
+
+        }
+
+        public MainWindowViewModel()
+        {
+
+            GetLCommand = new RelayCommand(OnGetLCommandExecute, CanGetLCommandExecuted);
+        }
+
+    }
+}
